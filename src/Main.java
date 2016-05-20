039a362d65d6aa3d81484fa3f711d19d8cf09a6b
mohammed.rampurawala <mohammed.rampurawala@hotmail.com>
2016-05-20 07:23:50 +0530

    Intellij changes

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("Hello WOrld");
-
-	}
-
+    public static void main(String[] args) {
+        System.out.println("Hello WOrld");
+    }
 }
