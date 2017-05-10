80df96bf396004f633c4aa61c04dc9bb5540a5b6
Mohammed Rampurawala <mohom.007@gmai.com>
2017-05-10 21:16:37 +0530

    Constants Updated.

--- src/com/data/misc/Constants.java
+++ src/com/data/misc/Constants.java
@@ -4,10 +4,5 @@ package com.data.misc;
  * Created by mohammed.rampurawala on 5/20/2016.
  */
 public class Constants {
-    public static final String ADB_STRING = "adb";
-    public static final String SHELL = "shell";
-    public static final String RUN_AS = "run-as ";
-    public static final long SLEEP_TIME = 50L;
-    public static String outputPath;
     public static String DEVICES = "devices";
 }
